4e10639d6fe65bd5d73b2cf806816a45b9af18f5
Gilang Mugni Respaty <gilmoregarland@gmail.com>
2021-08-27 06:57:44 +0700

    Add list

--- lib/puredocx/xml_generators/text.rb
+++ lib/puredocx/xml_generators/text.rb
@@ -5,16 +5,23 @@ module PureDocx
     class Text < Base
       DEFAULT_TEXT_SIZE  = 28
       DEFAULT_TEXT_ALIGN = 'left'.freeze
-      attr_reader :bold_enable, :italic_enable, :align, :size, :underline_enable
+      attr_reader :bold_enable, :italic_enable, :align, :size, :underline_enable,
+        :list_level, :list_type, :list_num_id
 
       def initialize(content, rels_constructor, arguments = {})
         super(nil, rels_constructor)
         @content          = CGI.escapeHTML(content)
         @bold_enable      = [*arguments[:style]].include?(:bold)
         @italic_enable    = [*arguments[:style]].include?(:italic)
-        @underline_enable = [*arguments[:style]].include?(:underline)
+        @underline_enable = [*arguments[:style]].include?(:underline) ? 'single' : ''
+        @list_type        = arguments.has_key?(:list_level) ? 'ListParagraph' : ''
+        @list_level       = arguments.has_key?(:list_level) ? arguments[:list_level] : ''
+        @list_num_id      = arguments.has_key?(:list_level) ? '2' : ''
         @align            = arguments[:align] || DEFAULT_TEXT_ALIGN
         @size             = arguments[:size]  || DEFAULT_TEXT_SIZE
+
+        puts "list level #{list_level}"
+        puts "list num id #{list_num_id}"
       end
 
       def params
@@ -24,6 +31,9 @@ module PureDocx
           '{BOLD_ENABLE}'      => bold_enable,
           '{ITALIC_ENABLE}'    => italic_enable,
           '{UNDERLINE_ENABLE}' => underline_enable,
+          '{LIST_TYPE}'        => list_type,
+          '{LIST_LEVEL}'       => list_level,
+          '{LIST_NUM_ID}'      => list_num_id,
           '{SIZE}'             => size
         }
       end
